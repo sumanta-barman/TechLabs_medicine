904ba01e755314c1a125e64b474deb63ab98f8b5
LumiKakuda <87920955+LumiKakuda@users.noreply.github.com>
2021-09-23 23:28:01 +0200

    Add files via upload

--- depression tweet GUI/depressionTweets.py
+++ depression tweet GUI/depressionTweets.py
@@ -15,12 +15,12 @@ X,y,bert_model = bert(df)
 root = tk.Tk()
 root.title("Depression Test")
 
-label = tk.Label(root,text="Hi! This program try to predic if you probably are or not depressed", width=100).pack(ipadx=50, ipady=50)
+label = tk.Label(root,text="Hi! This program try to predic if you probably are or not depressed", width=100).grid(ipadx=50, ipady=50)
 
-tweet = tk.Entry(root, selectborderwidth=5, text="Enter your tweet here", insertwidth="100").pack(ipadx=50, ipady=50)
+tweet = tk.Entry(root, selectborderwidth=5, text="Enter your tweet here", insertwidth="100").grid(ipadx=50, ipady=50)
 tweet = tweet.get()
 
 result = Button(root, text="Send", command=lambda: process(root, tweet, bert_model, X, y))
-result.pack()
+result.grid()
 
 root.mainloop()
