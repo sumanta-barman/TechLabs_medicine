7ba83a22f172d10605bed0c2412e756eb71150e0
LumiKakuda <87920955+LumiKakuda@users.noreply.github.com>
2021-09-23 22:34:57 +0200

    Add files via upload

--- depression tweet GUI/depressionTweets.py
+++ depression tweet GUI/depressionTweets.py
@@ -1,7 +1,6 @@
 #Imports
 import pandas as pd
 import tkinter as tk
-from tkinter import *
 from methods import * #class with functions
 
 #import DF
@@ -10,19 +9,17 @@ df = pd.read_csv("sentiment_tweets1.csv")
 #configuring DF
 df.rename(columns={"message to examine":"message"}, inplace=True)
 
+#preparing bert
+X,y,bert_model = bert(df)
+
 root = tk.Tk()
 root.title("Depression Test")
 
-canvas = tk.Canvas(root, width=300, height=400)
-canvas.pack()
+label = tk.Label(root,text="Hi! This program try to predic if you probably are or not depressed", width=100).pack(ipadx=50, ipady=50)
 
-label = tk.Label(root,text="Hi! Enter your tweet here").pack()
+tweet = tk.Entry(root, selectborderwidth=5, text="Enter your tweet here", insertwidth="100").pack(ipadx=50, ipady=50)
 
-tweet = tk.Entry(root, selectborderwidth=5)
-tweet.pack()
-tweet.insert(0,"")
-
-result = Button(root, text="Send", command=lambda: process(root, df, tweet))
+result = Button(root, text="Send", command=lambda: process(root, tweet, bert_model, X, y))
 result.pack()
 
 root.mainloop()
